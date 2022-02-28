6f20850cd5f8f1d922eec0d8f4133493c64d305c
neykhant <khantmaw48@gmail.com>
2022-02-28 15:56:50 +0630

    add navbar from online

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Contactus from "./pages/Contactus";
+import Products from "./pages/Products";
+import Services from "./pages/Services";
+import Signup from "./pages/Signup";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Home/>}/>
+          <Route path="/contactus" element={<Contactus/>}/>
+          <Route path="/products" element={<Products/>}/>
+          <Route path="/services" element={<Services/>}/>
+          <Route path="/signup" element={<Signup/>}/>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
